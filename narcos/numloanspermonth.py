6e3dd72b2c9d59321339e561f66a9aad1c89953c
agarwan1 <agarwan1192@gmail.com>
2018-03-31 14:54:49 -0400

    Added function

--- narcos/numloanspermonth.py
+++ narcos/numloanspermonth.py
@@ -19,25 +19,27 @@ df.describe()
 from datetime import datetime
 import pandas as pd
 
-df['date'] = pd.to_datetime(df['date'])
-df['year'], df['month'] = df['date'].dt.year, df['date'].dt.month
-#df
+def numloansmonth(df):
 
-df['mnth_yr'] = df['date'].apply(lambda x: x.strftime('%B-%Y'))
-#df.head()
+    df['date'] = pd.to_datetime(df['date'])
+    df['year'], df['month'] = df['date'].dt.year, df['date'].dt.month
+    #df
 
-df['mnth_yr2'] = df['date'].apply(lambda x: x.strftime('%Y-%m'))
-#df.head()
+    df['mnth_yr'] = df['date'].apply(lambda x: x.strftime('%B-%Y'))
+    #df.head()
 
-df.sort_values(by='date', inplace = True)
-#df.head()
+    df['mnth_yr2'] = df['date'].apply(lambda x: x.strftime('%Y-%m'))
+    #df.head()
 
-#Number of loans per month.
-df.groupby('mnth_yr2').agg('count')[['id']].plot.bar(y='id', figsize=(20,10))
-plt.title('Number of Loans per Month')
-plt.ylabel("Number of Loans")
-plt.xlabel("MonthYear")
-plt.savefig('loanspermonth.png')
-#df.plot(figsize=(20,4))
+    df.sort_values(by='date', inplace = True)
+    #df.head()
 
-plt.show()
+    #Number of loans per month.
+    df.groupby('mnth_yr2').agg('count')[['id']].plot.bar(y='id', figsize=(20,10))
+    plt.title('Number of Loans per Month')
+    plt.ylabel("Number of Loans")
+    plt.xlabel("MonthYear")
+    plt.savefig('loanspermonth.png')
+    #df.plot(figsize=(20,4))
+
+    plt.show()
