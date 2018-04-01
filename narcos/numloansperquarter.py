ba3a0167b38cd596714a9e2464f200e038fec1c3
agarwan1 <agarwan1192@gmail.com>
2018-04-01 17:41:14 -0400

    Added two graphs

--- narcos/numloansperquarter.py
+++ narcos/numloansperquarter.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 import seaborn as sns
 from datetime import datetime
 import pandas as pd
@@ -15,31 +16,26 @@ def numloansquarter():
     df['Qtr'] = pd.PeriodIndex(pd.to_datetime(df.mnth_yr2), freq='Q')
     df.head()
 
-    #Dataframe grouped by quarter. Takes the mediam loan amount per quarter.
-    # medloanq = df.groupby('Qtr').agg('median')[['loan_amount']]
-    # medloanq.head()
-
-    # medloanq.plot.bar(y='loan_amount', figsize=(20, 4))
-    # plt.title('Median Loan Amount Per Quarter')
-    # plt.ylabel("Loan Amount")
-    # plt.xlabel("Quarter")
-    # # plt.savefig('numloans.png')
     numloanq = df.groupby(['Qtr'])['id'].count().reset_index()
     numloanq = numloanq[numloanq['Qtr'] < pd.Period('2017Q3')]
-    numloanq
+    #numloanq
 
     numloanq.plot.line(x='Qtr', y='id', figsize=(20, 4), marker='o')
     plt.title('Number of Loans Per Quarter')
     plt.ylabel("Number of Loans")
     plt.xlabel("Quarter")
+    savepath = os.path.join('..', 'image')
+    plt.savefig(savepath + 'number_loans_per_quarter_line.png')
     plt.show()
 
     numloanq.plot.bar(x='Qtr', y='id', figsize=(20, 4))
     plt.title('Number of Loans Per Quarter')
     plt.ylabel("Number of Loans")
     plt.xlabel("Quarter")
+    savepath = os.path.join('..', 'image')
+    plt.savefig(savepath + 'number_loans_per_quarter_bar.png')
     plt.show()
 
-    plt.show()
+
 
 
